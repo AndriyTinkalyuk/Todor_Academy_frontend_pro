88d5d60e3bfc64f699b77ad996dddf5bf2bdb8dc
AndriyTinkalyuk <tinkalukandriy@gmail.com>
2025-04-30 00:30:46 +0300

    add Lesson 039

---
+++ Lesson 039/main.js
@@ -0,0 +1,6 @@
+import { capitalize } from './modules/stringUtils.js';
+import { getFirstElement } from './arrayUtils.js';
+
+console.log(capitalize("maybe baybe"));
+const myArray = [1, 2, 3, 4, 5];
+console.log(getFirstElement(myArray)); // 1
